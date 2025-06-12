110351f7d182d50c8e1dcbdceb9651256a4a0dd2
mikeballgra <42915782-mikeballgra@users.noreply.replit.com>
2025-06-12 20:48:08 +0000

    Restored to '803c6185f1568c95f0032c0926553554abaf85c1'
    Replit-Restored-To: 803c6185f1568c95f0032c0926553554abaf85c1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { ThemeProvider } from "@/components/theme-provider";
 import ShortcutsPage from "@/pages/shortcuts";
 
 function Router() {
@@ -17,14 +16,12 @@ function Router() {
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="shortcuts-ui-theme">
-      <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
 
